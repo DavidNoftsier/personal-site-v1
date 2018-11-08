c05be2a88c2475d1861ca6093a3d91759baf774a
David Noftsier <dnoftsier@gmail.com>
2018-11-08 11:33:14 -0500

    Styling PersonalInfo component as a css grid, adding content to it

--- src/components/PersonalInfo/PersonalInfo.js
+++ src/components/PersonalInfo/PersonalInfo.js
@@ -5,9 +5,14 @@ export class PersonalInfo extends React.Component{
 	render(){
 		return(
 			<div className={styles['personal-info-container']}>
-				<div>David Noftsier</div>
-				<div>Waltham, MA</div>
-				<div>() ()</div>
+				<div className={styles['name']}>David Noftsier</div>
+				<div className={styles['location']}>Waltham, MA</div>
+				<div className={styles['links']}>() ()</div>
+				<div className={styles['title']}>Software Developer</div>
+				<div className={styles['degree']}>B.A. Computer Science, University at Albany</div>
+				<div className={styles['tech']}>JS, CSS3, HTML5, C#, SQL</div>
+				<div className={styles['email']}>dnoftsier@gmail.com</div>
+				<div className={styles['pic']}>PIC</div>
 			</div>
 		);
 	}
