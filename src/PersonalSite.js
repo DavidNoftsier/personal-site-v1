d5d2cc8ebe1b8b7d5ba3be3ebc17fc5ec09d1989
David Noftsier <dnoftsier@gmail.com>
2018-11-08 12:18:01 -0500

    Adding ProjectCard component

--- src/PersonalSite.js
+++ src/PersonalSite.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import styles from './PersonalSite.module.css';
 import { WelcomeMessage } from './components/WelcomeMessage/WelcomeMessage.js';
 import { PersonalInfo } from './components/PersonalInfo/PersonalInfo.js';
+import { ProjectCard } from './components/ProjectCard/ProjectCard.js';
 
 class PersonalSite extends Component {
   render() {
@@ -10,6 +11,7 @@ class PersonalSite extends Component {
       <div>
         <WelcomeMessage></WelcomeMessage>
         <PersonalInfo></PersonalInfo>
+        <ProjectCard></ProjectCard>
       </div>
     );
   }
---
+++ src/components/ProjectCard/ProjectCard.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import styles from './ProjectCard.module.css';
+
+export class ProjectCard extends React.Component{
+	render(){
+		return(
+			<div className={styles['project-card']}>
+				<div className={styles['name']}>Goodbye HTML</div>
+				<div className={styles['date']}>August 2018</div>
+				
+				<div><label for='description'>Description:</label>
+					<div id='description' className={styles['description']}>
+						Goodbye HTML is a Google Chrome extension that allows users to block elements from appearing
+						when they visit a website. Multiple options exist for blocking elements temporarily or permanently
+						on limited sites or all sites. A robust UI was developed to allow the use of mouse movemenets 
+						to discover and choose elements.
+					</div>
+				</div>
+				<div className={styles['tech']}>Chrome Extension, CSS, JS, HTML</div>
+			</div>
+		);
+	}
+}
