dfba476d8c4ee4644816bca6d3aff81c03512e87
David Noftsier <dnoftsier@gmail.com>
2018-11-08 09:14:28 -0500

    Updating whitespace in WelcomeMessage.js

--- src/components/WelcomeMessage.js
+++ src/components/WelcomeMessage.js
@@ -3,12 +3,12 @@ import './WelcomeMessage.css';
 
 export class WelcomeMessage extends React.Component{
 	render(){
-		    return(
-			    <div class="welcome-message-container">
-	          <div class="text-left medium">Hello my name is</div>
-	          <div class="text-center extra-large">David Noftsier</div>
-	          <div class="text-right medium">Lets create together</div>
-	        </div>
-				);
+		return(
+			<div class="welcome-message-container">
+				<div class="text-left medium">Hello my name is</div>
+				<div class="text-center extra-large">David Noftsier</div>
+				<div class="text-right medium">Lets create together</div>
+			</div>
+		);
 	}
 }
