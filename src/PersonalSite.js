41dcc1b6dd4ac99eec15779b06139afe1b91df7a
David Noftsier <dnoftsier@gmail.com>
2018-11-08 13:33:18 -0500

    Update semantic structure of PersonalSite HTML

--- src/PersonalSite.js
+++ src/PersonalSite.js
@@ -9,33 +9,37 @@ class PersonalSite extends Component {
   render() {
     return (
 		<div>
-			<WelcomeMessage
-				name="David"
-			>
-			</WelcomeMessage>
-			<PersonalInfo
-				name="David Noftsier"
-				location="Waltham, MA"
-				github="git"
-				linkedIn="linkedin"
-				title="Software Developer"
-				degree="B.A. Computer Science, University at Albany"
-				tech="JS, CSS3, HTML5, C#, SQL"
-				email="dnoftsier@gmail.com"
-				pic="PIC"
-			>
-			</PersonalInfo>
-			<ProjectCard 
-				name="Goodbye HTML"
-				date="August 2018"
-				description="Goodbye HTML is a Google Chrome extension that allows users to block elements from appearing
-							when they visit a website. Multiple options exist for blocking elements temporarily or permanently
-							on limited sites or all sites. A robust UI was developed to allow the use of mouse movemenets 
-							to discover and choose elements."
-				tech="Chrome Extension, CSS, JS, HTML"
-			>
-			</ProjectCard>
-		</div>
+			<header>
+		        <WelcomeMessage
+		        	name="David"
+		        >
+		        </WelcomeMessage>
+	        </header>
+	        <main>
+		        <PersonalInfo
+		        	name="David Noftsier"
+		        	location="Waltham, MA"
+		        	github="git"
+		        	linkedIn="linkedin"
+		        	title="Software Developer"
+		        	degree="B.A. Computer Science, University at Albany"
+		        	tech="JS, CSS3, HTML5, C#, SQL"
+		        	email="dnoftsier@gmail.com"
+		        	pic="PIC"
+		        >
+		        </PersonalInfo>
+		        <ProjectCard 
+		        	name="Goodbye HTML"
+		        	date="August 2018"
+		        	description="Goodbye HTML is a Google Chrome extension that allows users to block elements from appearing
+								when they visit a website. Multiple options exist for blocking elements temporarily or permanently
+								on limited sites or all sites. A robust UI was developed to allow the use of mouse movemenets 
+								to discover and choose elements."
+					tech="Chrome Extension, CSS, JS, HTML"
+		        >
+		        </ProjectCard>
+	        </main>
+      	</div>
     );
   }
 }
