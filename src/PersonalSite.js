1109f25cd4d202dd666a114a723693df3ddb0bc8
David Noftsier <dnoftsier@gmail.com>
2018-11-08 09:58:43 -0500

    Update css import statement in PersonalSite component

--- src/PersonalSite.js
+++ src/PersonalSite.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './PersonalSite.module.css';
+import styles from './PersonalSite.module.css';
 import { WelcomeMessage } from './components/WelcomeMessage/WelcomeMessage.js';
 
 class PersonalSite extends Component {
