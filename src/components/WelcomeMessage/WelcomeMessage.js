170d8b25f6a4aada4a8ea010e7f8cca2e7ab5ea9
David Noftsier <dnoftsier@gmail.com>
2018-11-19 22:57:05 -0500

    Adding basic animation to the WelcomeMessage

--- src/components/WelcomeMessage/WelcomeMessage.js
+++ src/components/WelcomeMessage/WelcomeMessage.js
@@ -6,14 +6,20 @@ class WelcomeMessage extends React.Component{
 	render(){
 		return(
 			<section aria-label="Welcome Message" className={styles["welcome-message-container"]}>
-				<span className={[styles["text-left"], styles["medium"], styles["block-formatting"]].join(' ')}>
+				<span className={[styles["text-left"], styles["medium"], styles["block-formatting"], styles["greeting-1"]].join(' ')}>
 					Hi!
 				</span>
 				<span className={["text-center", styles["extra-large"], styles["block-formatting"]].join(' ')}>
-					I'm {this.props.name}
+					<span className={styles["greeting-2"]}>I'm </span> 
+					<span className={styles["greeting-3"]}>{this.props.name}</span>
 				</span>
 				<span className={["text-right", styles["medium"], styles["block-formatting"]].join(' ')}>
-					Lets build stuff <span aria-label="Smiley face emoji">:)</span>
+					<span className={styles["greeting-4"]}>Lets </span>
+					<span className={styles["greeting-5"]}>build </span>
+					<span className={styles["greeting-6"]}>stuff </span>
+					<span aria-label="Smiley face emoji" className={styles["greeting-7"]}>
+						<span aria-hidden="true" className={styles["greeting-smiley"]}></span>
+					</span>
 				</span>
 			</section>
 		);
