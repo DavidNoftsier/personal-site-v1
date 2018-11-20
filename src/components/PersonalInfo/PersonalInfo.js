30297e3f8937f1e8e064b6812ad2103f0939c2fd
David Noftsier <dnoftsier@gmail.com>
2018-11-20 08:33:45 -0500

    Reworking design of PersonalInfo

--- src/components/PersonalInfo/PersonalInfo.js
+++ src/components/PersonalInfo/PersonalInfo.js
@@ -7,20 +7,30 @@ class PersonalInfo extends React.Component{
 		return(
 			<div className={styles['personal-info-page']}>
 				<section aria-label="Personal Information" className={styles['personal-info-container']}>
-					<div className={styles['name']}>
-						<div aria-label="First Name">{this.props.firstName}</div>
-						<div aria-label="Last Name">{this.props.lastName}</div>
+					
+					<div className={styles['personal-info-left']}>
+						<div className={styles['name']}>
+							<span aria-label="First Name">{this.props.firstName} </span>
+							<span aria-label="Last Name">{this.props.lastName}</span>
+						</div>
+						<div aria-label="Job Title" className={styles['title']}>{this.props.title}</div>
+						{/*<div aria-label="Headshot" className={styles['pic']}>{this.props.pic}</div>*/}
 					</div>
-					<div aria-label="Location" className={styles['location']}>{this.props.location}</div>
-					<section aria-label="External Links" className={styles['links']}>
-						<a aria-label="Github" title={this.props.github} href={this.props.github} target="_blank"> Github</a>
-						<a aria-label="LinkedIn" title={this.props.linkedIn} href={this.props.linkedIn} target="_blank"> LinkedIn</a>
-					</section>
-					<div aria-label="Job Title" className={styles['title']}>{this.props.title}</div>
-					<div aria-label="Degree" className={styles['degree']}>{this.props.degree}</div>
-					<div aria-label="Technologies used" className={styles['tech']}>{this.props.tech}</div>
-					<div aria-label="Email" className={styles['email']}>{this.props.email}</div>
-					<div aria-label="Headshot" className={styles['pic']}>{this.props.pic}</div>
+
+					<div className={styles['personal-info-right']}>
+						<div aria-label="Email" className={styles['email']}>
+							<a href={"mailto:" + this.props.email}>{this.props.email}</a>
+						</div>
+						
+						<div aria-label="Location" className={styles['location']}>{this.props.location}</div>
+						<section aria-label="External Links" className={styles['links']}>
+							<a aria-label="Github" title={this.props.github} href={this.props.github} target="_blank"> Github</a>
+							<a aria-label="LinkedIn" title={this.props.linkedIn} href={this.props.linkedIn} target="_blank"> LinkedIn</a>
+						</section>
+						<div aria-label="Degree" className={styles['degree']}>{this.props.degree}</div>
+						<div aria-label="Technologies used" className={styles['tech']}>{this.props.tech}</div>
+					</div>
+
 				</section>
 			</div>
 		);
